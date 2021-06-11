a231910a78f3aeeaec0f0164005c4ec11358e370
Tbonexas <theclash090@gmail.com>
2021-06-11 14:13:17 -0700

    added more directories, chnages headers margin

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,12 +1,9 @@
 import React from 'react' 
-import App from '../../App';
-import Header from '../../components/Header';
 import './style.css';
 
 const Home = props => {
     return (
         <div>
-            <Header />  
         </div>
     );
 }
